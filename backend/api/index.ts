bcdc302f24d03f1f51609ef7cbc17f8435a09d7a
Docterz Dev <docterz@gmail.com>
2026-03-22 10:44:20 +0530

    fix: explicit CORS handling in vercel api wrapper

--- backend/api/index.ts
+++ backend/api/index.ts
@@ -4,7 +4,13 @@ import { AppModule } from '../src/app.module';
 export default async (req: any, res: any) => {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api/v1');
-  app.enableCors();
+  app.enableCors({
+    origin: [
+      'https://cric-xi.vercel.app',
+      'http://localhost:3000'
+    ],
+    credentials: true,
+  });
   await app.init();
   const instance = app.getHttpAdapter().getInstance();
   instance(req, res);
